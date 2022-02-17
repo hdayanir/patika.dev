148a0095070ed234e270fcfc271fcc0cf3467967
Huseyin Dayanir <hsyndayanir@gmail.com>
2022-02-17 11:34:07 +0300

    Summary

---
+++ C#_101/practices/error_handling/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace error_handling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayi : " + sayi);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata : "+ ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı");
+            }
+            */
+            /*
+            try
+            {
+                //int a= int.Parse(null);
+            
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+            }
+            */
+            /*
+            try
+            {
+                int a= int.Parse("test");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değildir.");
+                Console.WriteLine(ex);
+            }
+            */
+
+            try
+            {
+                int a= int.Parse("-20000000000");
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük veya çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı");
+            }
+        }      
+    }
+}
