ce6648b18722a4cca6348d9419ab1ee2513dc4f0
Huseyin Dayanir <hsyndayanir@gmail.com>
2022-03-13 15:43:53 +0300

    Summary

---
+++ C#_101/practices/sinif_kavrami/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz dizini
+            // class SinifAdi
+            // {
+            //      [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+            //      [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+            //      {
+            //          Metot komutları
+            //      }    
+            // }
+
+            // Erişim belirleyiciler
+            // * Public         Her yerde erişilebilir.
+            // * Private        Sadece tanımlandığı sınıf içerinde erişilebilir.
+            // * Internal       Sadece bulunduğu proje içerisinde (yani ...cs içerisinde) erişilebilir.
+            // * Protected      Sadece tanımlandığı sınıfta veya o sınıftan miras alan diğer sınıflarda kullanılabilir.
+
+            Calisan calisan1= new Calisan();
+            calisan1.Ad ="Huseyin";
+            calisan1.Soyad="DAYANIR";
+            calisan1.no=123;
+            calisan1.Departman="Backend";
+            calisan1.CalisanBilgileri();
+
+            System.Console.WriteLine("*****");
+            
+            Calisan calisan2= new Calisan();
+            calisan2.Ad ="Ali";
+            calisan2.Soyad="VELİ";
+            calisan2.no=456;
+            calisan2.Departman="Frontend";
+            calisan2.CalisanBilgileri();
+
+
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int no;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            System.Console.WriteLine("Çalışan Adı        : " + Ad);
+            System.Console.WriteLine("Çalışan Soyadı     : " + Soyad);
+            System.Console.WriteLine("Çalışan Numarası   : " + no);
+            System.Console.WriteLine("Çalışan Departmanı : " + Departman);
+        }
+
+    }
+}
