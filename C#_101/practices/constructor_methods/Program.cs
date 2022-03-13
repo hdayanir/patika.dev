823e6fc629bc65d4285600f8017f27dcfc9e3800
Huseyin Dayanir <hsyndayanir@gmail.com>
2022-03-13 16:37:50 +0300

    Summary

---
+++ C#_101/practices/constructor_methods/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace constructor_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz dizini
+            // class SinifAdi
+            // {
+            //      [Erişim Belirleyici] [Veri Tipi] OzellikAdi;
+            //      [Erişim Belirleyici] [Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+            //      {
+            //          Metot komutları
+            //      }    
+            // }
+
+            // Erişim belirleyiciler
+            // * Public         Her yerde erişilebilir.
+            // * Private        Sadece tanımlandığı sınıf içerinde erişilebilir.
+            // * Internal       Sadece bulunduğu proje içerisinde (yani ...cs içerisinde) erişilebilir.
+            // * Protected      Sadece tanımlandığı sınıfta veya o sınıftan miras alan diğer sınıflarda kullanılabilir.
+
+            Calisan calisan1= new Calisan();
+            calisan1.Ad ="Huseyin";
+            calisan1.Soyad="DAYANIR";
+            calisan1.No=123;
+            calisan1.Departman="Backend";
+            calisan1.CalisanBilgileri();
+
+            System.Console.WriteLine("***************************************");
+            
+            Calisan calisan2= new Calisan();
+            calisan2.Ad ="Ali";
+            calisan2.Soyad="VELİ";
+            calisan2.No=456;
+            calisan2.Departman="Frontend";
+            calisan2.CalisanBilgileri();
+
+            System.Console.WriteLine("***************************************");
+
+            Calisan calisan3= new Calisan("x","Y",789,"Full Stack");
+            calisan3.CalisanBilgileri();
+
+            System.Console.WriteLine("***************************************");
+
+            Calisan calisan4= new Calisan("a","B");
+            calisan4.CalisanBilgileri();
+
+
+
+        }
+    }
+
+    class Calisan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+
+        public Calisan(string ad, string soyad,int no, string departman)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.No = no;
+            this.Departman = departman;
+        }
+
+        public Calisan(string ad, string soyad)
+        {
+            this.Ad = ad;
+            this.Soyad = soyad;
+            
+        }
+
+        public Calisan()
+        {
+
+        }
+        public void CalisanBilgileri()
+        {
+            System.Console.WriteLine("Çalışan Adı        : " + Ad);
+            System.Console.WriteLine("Çalışan Soyadı     : " + Soyad);
+            System.Console.WriteLine("Çalışan Numarası   : " + No);
+            System.Console.WriteLine("Çalışan Departmanı : " + Departman);
+        }
+
+    }
+}
---
+++ C#_101/practices/dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<int,string> kullanicilar = new Dictionary<int,string>();  //Key integer olsun, value si ise string olsun. 
+            kullanicilar.Add(10,"Huseyin DAYANIR"); // ilk deger key, ikinci deger ise value
+            kullanicilar.Add(12,"a B");
+            kullanicilar.Add(14,"c D");
+            kullanicilar.Add(16,"e F");
+
+            //Dizinin elemalarına erişim
+            System.Console.WriteLine("***** Dizinin elemanlarına erişim *****");
+            System.Console.WriteLine(kullanicilar[12]);//12 key'ine sahip değeri getir.
+            foreach (var item in kullanicilar)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //Count
+            System.Console.WriteLine("***** Count *****");
+            System.Console.WriteLine(kullanicilar.Count);
+
+            //Contains
+            System.Console.WriteLine("***** Contains *****");
+            System.Console.WriteLine(kullanicilar.ContainsKey(12));
+            System.Console.WriteLine(kullanicilar.ContainsValue("a B"));
+
+            //Remove
+            System.Console.WriteLine("***** Remove *****");
+            kullanicilar.Remove(16);
+            foreach (var item in kullanicilar)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //Keys
+            System.Console.WriteLine("***** Keys *****");
+            foreach (var item in kullanicilar.Keys)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //Values
+            System.Console.WriteLine("***** Values *****");
+            foreach (var item in kullanicilar.Values)
+            {
+                System.Console.WriteLine(item);
+            }
+
+        }
+    }
+}
