0e76569c38b5238072de837c931f7711dd5afecb
Hüseyin DAYANIR <hsyndayanir@gmail.com>
2023-12-10 23:22:19 +0300

    Summary

---
+++ C#_Projects/DrawTriangle/DrawTriangle/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Üçgenin boyutunu girin: ");
+                int boyut = int.Parse(Console.ReadLine());
+
+                DrawTriangle(boyut);
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz bir giriş. Lütfen bir tam sayı girin.");
+            }
+        }
+
+        static void DrawTriangle(int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= size - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
